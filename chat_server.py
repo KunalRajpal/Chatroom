479b0c3065175b90966d5e27032b10c8468950cb
Kunal <rajpal.kunal1@gmail.com>
2022-10-19 22:24:57 -0500

    minor changes

--- chat_server.py
+++ chat_server.py
@@ -13,14 +13,17 @@ SERVER_HOST = "127.0.0.1" #Local host
 SERVER_PORT = 8080
 
 # server set up
+#create a server connection
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # The fir parameter indicates we are using internet socket and the second parameter indicates we are using TCP and not UDP
 server.bind((SERVER_HOST, SERVER_PORT)) #bind it to our (local) host with a tuple containing our host and port
+
+
 server.listen() #listening mode
 
 #Lists
-clients = []        #list of clients connecting to the server
-client_names = []   #list of client nicknames
-chat_rooms = []     #list of all existing chart rooms
+list_of_clients = []        #list of clients connecting to the server
+client_names = []           #list of client nicknames
+chat_rooms = []             #list of all existing chart rooms
 
 
 MAXIMUM_CAPACITY = 5#Max capacity of the chat rooms
