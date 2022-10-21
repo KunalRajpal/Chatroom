96cc8e03138104963fe41fb8f5a7d6a9c627b376
Kunal <rajpal.kunal1@gmail.com>
2022-10-20 20:39:29 -0500

    Almost done

--- chat_client.py
+++ chat_client.py
@@ -8,10 +8,14 @@ import json
 import socket
 import threading
 
+
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 8080
 
-nickname = input("Please enter the nickname you wish to use: ")
+welcome = "Welcome! To the awesome chat room\nLet's get Started\n************************************"
+print (welcome)
+
+nickname = input("Please enter the nickname you wish to be identified with: ")
 
 #setting up TCp with our server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,28 +28,125 @@ introduction  = {
 
 # as soon as the server is joined our client send out a new nick name
 client.send(json.dumps(introduction).encode())
-new = client.recv(1024)
-print(json.loads(new).decode('UTF-8'))
 
-""" def write():
+def handle_messages():
     while True:
         try:
-            trystuff()
+            message = client.recv(1024)
+
+            data_json = json.loads(message.decode('UTF-8'))
+
+            if data_json["reply-type"] == "welcome-reply":
+
+                print(data_json["output"])
+
+            if data_json["reply-type"] == "existing-rooms-reply":
+
+                print(data_json["list_of_rooms"])
+
+            if data_json["reply-type"] == "room-info-reply":
+
+                print(data_json["output"])
+                print(data_json["peers"])
+
+            if data_json["reply-type"] == "join-room-reply":
+                print(data_json["output"])
+                print(data_json["peers"])
+
+            if data_json["reply-type"] == "create-room-reply":
+
+                print(data_json["output"])
+
+
+            if data_json["reply-type"] == "send-message-reply":
+
+                print(data_json["list_of_rooms"])
+            
+            if data_json["reply-type"] == "leave-room-reply":
+
+                print(data_json["output"])
+                print(data_json["list_of_rooms"])
+
         except Exception as e:
-            continue """
+            continue
 
 
-""" def read():
-    while True:
-        try:
-            trystuff()
-        except Exception as e:
-            continue """
 
 
-""" # Starting Threads For Listening And Writing
-read_thread = threading.Thread(target=read)
-read_thread.start()
 
-write_thread = threading.Thread(target=write)
-write_thread.start() """
+handler = threading.Thread(target = handle_messages)
+handler.start()
+
+cmd = """This is the list of commands you can utilize:\n
+[*]ROOMS: to get a list of all existing chat rooms
+[*]ROOM-INFO: Use this command followed by the name of the room to get the number and list of all connected users for the room
+[*]JOIN-ROOM: Use this command followed by the name of the room you wish to join to join the room
+[*]CREATE-ROOM: Use this command followed by the name of the room you wish to create
+[*]LEAVE-ROOM: Use this command followed by the name of the room you wish to leave
+[*]SEND: Use this command followed by the name of the chatroom you wish to send your message in and your message"""
+
+print(cmd)
+
+while True:
+    command = input("Enter Command: ")
+    words = command.split(" ")
+    n = len(words)
+    if n <=0:
+        print("Empty command entered")
+    else:
+        if words[0] == "ROOMS":
+
+            request = {
+                "type":"existing-rooms"
+            }
+
+            client.send(json.dumps(request).encode())
+
+        elif words[0] == "ROOM-INFO":
+
+            request = {
+                "type":"room-info",
+                "room-name":words[1]
+            }
+
+            client.send(json.dumps(request).encode())
+
+        elif words[0] == "JOIN-ROOM":
+
+            request = {
+                "type":"join-room",
+                "nick-name":nickname,
+                "room-name":words[1]
+            }
+
+            client.send(json.dumps(request).encode())
+
+        elif words[0] == "CREATE-ROOM":
+
+            request = {
+                "type":"create-room",
+                "nick-name":nickname,
+                "room-name":words[1]
+            }
+
+            client.send(json.dumps(request).encode())
+
+        elif words[0] == "LEAVE-ROOM":
+
+            request = {
+                "type":"leave-room",
+                "nick-name":nickname,
+                "room-name":words[1]
+            }
+
+            client.send(json.dumps(request).encode())
+
+        elif words[0] == "SEND":
+
+            request = {
+                "type": ""
+            }
+
+            client.send(json.dumps(request).encode())
+        else:
+            print("Invalid command entered, please try again!")
--- chat_server.py
+++ chat_server.py
@@ -26,7 +26,7 @@ print("Listening on %s:%d" % (SERVER_HOST, SERVER_PORT))
 #Lists
 list_of_clients = []        #list of clients connecting to the server
 client_nicknames = []       #list of client nicknames
-chat_rooms = []             #list of all existing chart rooms
+chat_rooms = {}             #list of all existing chart rooms
 
 
 #Functions
@@ -47,54 +47,91 @@ def remove(thisClient):
         list_of_clients.remove(thisClient)
 
 def welcome(newNickName, conn):
-    print("A new user joined the chat! Welcome: " + newNickName)
-    client_nicknames.append(newNickName)
+
+    if newNickName in client_nicknames:
+        msg = "Error: Sorry the nickname already exists!"
+    else:
+        msg = "*****************************************\n\nHello %s! Welcome to the awesome chat room, we hope you brought pizza\n\n*****************************************"%newNickName
+        print("A new user joined the chat! Welcome: " + newNickName)
+        client_nicknames.append(newNickName)
 
     reply = {
         "reply-type":"welcome-reply",
-        "msg": "Hello! Welcome to the awesome chat room, we hope you brought pizza XD" #TODO
+        "output": msg 
     }
 
     conn.send(json.dumps(reply).encode())
-    print("reply sent")
 
 
 def existing_rooms(conn):
     
     reply = {
         "reply-type":"existing-rooms-reply",
-        "list_of_rooms": chat_rooms
+        "list_of_rooms": chat_rooms # you will need to parse this in the client side to get a list of all the names of the chat rooms #TODO
     }
 
     conn.send(json.dumps(reply).encode())
-    print(chat_rooms)
 
-def room_info(conn):
+def room_info(conn, room):
+    if room in chat_rooms:
+        theList = chat_rooms[room]
+        length = len(theList)
+        msg = "The chat room %s has %d users"%(room, length)
+    else:
+        msg = "The room does not exist"
 
     reply = {
-        "reply-type":"room-info-reply"
+        "reply-type":"room-info-reply",
+        "peers": theList,
+        "output": msg
     }
 
     conn.send(json.dumps(reply).encode())
-    print("reply sent")
 
-def join_room(conn):
+
+def join_room(conn, roomToJoin, nick):
+
+    if roomToJoin in chat_rooms:
+        #we join
+        theList = chat_rooms[roomToJoin]
+        length = len(theList)
+        if length <5:
+            #we can join
+            theList.append(nick)
+            msg= "Room joining successful. Now you're in the chat room %s"%roomToJoin
+        else:
+            msg = "Room joining Failed: The room has reached its maximum capacity"
+    else:
+        msg= "Room joining Failed. The room does not exist, please try again" #TODO
 
     reply = {
-        "reply-type":"join-room-reply"
+        "reply-type":"join-room-reply",
+        "list_of_rooms": chat_rooms,
+        "peers": theList,
+        "output": msg
     }
 
     conn.send(json.dumps(reply).encode())
-    print("reply sent")
 
-def create_room(conn, newRoom):
+def create_room(conn, room_name, nick):
+
+    if room_name in chat_rooms:
+        
+        msg = "Please pick a different room name, another room with the same name already exists"
+
+    else:
+        #add the name as the key and the list of nicknames as value in the dictionary
+        chat_rooms[room_name] = []
+        msg = "The room %s has been created. You can now look at the updated list of chat rooms using the appropriate command"%room_name
 
     reply = {
-        "reply-type":"create-room-reply"
+        "reply-type":"create-room-reply",
+        "list_of_rooms": chat_rooms,
+        "output": msg
     }
 
     conn.send(json.dumps(reply).encode())
-    print("reply sent")
+    print("A new room %s has been created by the user %s"%room_name%nick)
 
 def send_message(conn):
 
@@ -106,14 +143,22 @@ def send_message(conn):
     print("reply sent")
 
 
-def leave_room(conn):
+def leave_room(conn, roomToBeLeft, nick):
+    
+    if roomToBeLeft in chat_rooms:
+        if nick in chat_rooms[roomToBeLeft]:
+            (chat_rooms[roomToBeLeft]).remove(nick)
+            msg = "Leaving room successful"      
+    else:
+        msg = "The room does not exist"
 
     reply={
-        "reply-type":"leave-room-reply"
+        "reply-type":"leave-room-reply",
+        "list_of_rooms": chat_rooms,
+        "output": msg
     }
 
     conn.send(json.dumps(reply).encode())
-    print("reply sent")
 
 def client_handler(client, address):
 
@@ -123,7 +168,7 @@ def client_handler(client, address):
 
             data_json = json.loads(message.decode('UTF-8'))
 
-            if data_json["type"] == "welcome":
+            if data_json["type"] == "welcome": 
                 welcome(data_json["nick-name"], client)
 
             if data_json["type"] == "existing-rooms":
@@ -132,15 +177,15 @@ def client_handler(client, address):
 
             if data_json["type"] == "room-info":
                 #view the number and list of connected users for each room
-                room_info(client)
+                room_info(client, data_json["room-name"])
 
             if data_json["type"] == "join-room":
                 #Join existing chat rooms if the room capacity is not full
-                join_room(client)
+                join_room(client, data_json["room-name"], data_json["nick-name"])
 
             if data_json["type"] == "create-room":
                 #Create chat-rooms
-                create_room(client, roomName)
+                create_room(client, data_json["room-name"], data_json["nick-name"])
 
             if data_json["type"] == "send-message":
                 #Send messages to chat-rooms
@@ -148,7 +193,7 @@ def client_handler(client, address):
             
             if data_json["type"] == "leave-room":
                 #Leave a chat-room
-                leave_room(client)
+                leave_room(client, data_json["room-name"], data_json["nick-name"])
 
         except Exception as e:
             continue
